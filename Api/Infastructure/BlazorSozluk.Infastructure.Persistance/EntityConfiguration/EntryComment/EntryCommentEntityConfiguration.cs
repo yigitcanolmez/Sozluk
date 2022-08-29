87101d4d953a313dcec1f7c81fd6096e1657a21c
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-08-29 11:09:23 +0300

    Entity-Configuration

---
+++ Api/Infastructure/BlazorSozluk.Infastructure.Persistance/EntityConfiguration/EntryComment/EntryCommentEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BlazorSozluk.Infastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infastructure.Persistance.EntityConfiguration.EntryComment
+{
+    public class EntryCommentEntityConfiguration : BaseEntityConfiguration<BlazorSozlukApi.Domain.Models.EntryComment>
+    {
+        public override void Configure(EntityTypeBuilder<BlazorSozlukApi.Domain.Models.EntryComment> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("entrycomment", BlazorSozlukContext.DEFAULT_SCHEMA);
+
+            builder.HasOne(i => i.CreatedBy)
+                .WithMany(i => i.EntryComment)
+                .HasForeignKey(i => i.CreatedById);
+
+            builder.HasOne(i => i.Entry)
+                .WithMany(i => i.EntryComments)
+                .HasForeignKey(i => i.EntryId);
+        }
+
+    }
+}
