ef30e53134469047786ff34e6d168b16bef75310
Yiğitcan Ölmez <yigitcan.olmez@affinitybox.com>
2022-09-06 17:57:05 +0300

    BS-12

--- Clients/BlazorWeb/BlazorSozluk.WebApp/Program.cs
+++ Clients/BlazorWeb/BlazorSozluk.WebApp/Program.cs
@@ -1,4 +1,7 @@
+using Blazored.LocalStorage;
 using BlazorSozluk.WebApp;
+using BlazorSozluk.WebApp.Infastructure.Services;
+using BlazorSozluk.WebApp.Infastructure.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,26 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("WebApiClient",client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["https://localhost:5001"]);
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
+    return clientFactory.CreateClient("WebApiClient");
+});
+
+builder.Services.AddTransient<IVoteServices, VoteServices>();
+builder.Services.AddTransient<IEntryService,EntryService>();
+builder.Services.AddTransient<IFavServices,FavServices>();
+builder.Services.AddTransient<IIdentityService,IdentityService>();
+builder.Services.AddTransient<IUserService,UserService>();
+
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
